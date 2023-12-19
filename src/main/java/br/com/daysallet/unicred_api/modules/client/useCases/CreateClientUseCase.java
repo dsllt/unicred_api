7a163fec12646c9009af2959e3d99f5a3bce0f3a
dsllt <dayanesallet@hotmail.com>
2023-12-19 15:36:15 -0300

    Defines a password encoder

--- src/main/java/br/com/daysallet/unicred_api/modules/client/useCases/CreateClientUseCase.java
+++ src/main/java/br/com/daysallet/unicred_api/modules/client/useCases/CreateClientUseCase.java
@@ -1,6 +1,7 @@
 package br.com.daysallet.unicred_api.modules.client.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.daysallet.unicred_api.exceptions.ClientAlreadyExists;
@@ -12,11 +13,17 @@ public class CreateClientUseCase {
 
   @Autowired
   private ClientRepository clientRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
   
   public ClientEntity execute(ClientEntity clientEntity){
     this.clientRepository.findByEmailOrCnpj(clientEntity.getEmail(), clientEntity.getCnpj()).ifPresent((client) -> {
       throw new ClientAlreadyExists();
     });
+
+    var password = passwordEncoder.encode(clientEntity.getPassword());
+    clientEntity.setPassword(password);
 
     return this.clientRepository.save(clientEntity);
   }
--- src/main/java/br/com/daysallet/unicred_api/security/SecurityConfig.java
+++ src/main/java/br/com/daysallet/unicred_api/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package br.com.daysallet.unicred_api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,4 +18,9 @@ public class SecurityConfig {
     });
     return http.build();
 }
+
+  @Bean
+  public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
 }
