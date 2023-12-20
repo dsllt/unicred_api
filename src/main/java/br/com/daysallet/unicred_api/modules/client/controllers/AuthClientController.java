d44f174ae81198d1d7230835834051ccc145d414
dsllt <dayanesallet@hotmail.com>
2023-12-20 11:52:48 -0300

    Creates a security filter to verify user authentification and gives permission to access certain routes

--- src/main/java/br/com/daysallet/unicred_api/modules/client/controllers/AuthClientController.java
+++ src/main/java/br/com/daysallet/unicred_api/modules/client/controllers/AuthClientController.java
@@ -1,7 +1,5 @@
 package br.com.daysallet.unicred_api.modules.client.controllers;
 
-import javax.naming.AuthenticationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/br/com/daysallet/unicred_api/security/SecurityConfig.java
+++ src/main/java/br/com/daysallet/unicred_api/security/SecurityConfig.java
@@ -1,23 +1,28 @@
 package br.com.daysallet.unicred_api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+  @Autowired
+  private SecurityFilter securityFilter;
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
       auth.requestMatchers("/client/").permitAll()
           .requestMatchers("/auth/client").permitAll();
-      
       auth.anyRequest().authenticated();
-    });
+    })
+    .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
     return http.build();
 }
 
